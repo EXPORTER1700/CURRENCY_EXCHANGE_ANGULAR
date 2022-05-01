5863eba5b02aa7a45cc98fe612763da1c71cf413
EXPORTER1700 <i.stetsenko1998@gmail.com>
2022-05-01 13:37:22 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+export interface Currency {
+  r030: number
+  txt: string
+  rate: number
+  cc: string
+  exchangedate: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-edu';
+export class AppComponent implements OnInit {
+  currencies: Currency[] = []
+  headerCurrencies: Currency[] = []
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<Currency[]>('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+      .subscribe(response => {
+        this.currencies = response
+        this.headerCurrencies = response.filter(currency => currency.cc === 'USD' || currency.cc === 'EUR')
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {HeaderComponent} from './header/header.component';
+import {FormsModule} from "@angular/forms";
+import { ConverterComponent } from './converter/converter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConverterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
